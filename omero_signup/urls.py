b063b6a71e81afed25a756a5bb6c228102ab25d6
Will Moore <w.moore@dundee.ac.uk>
2020-01-16 09:23:56 +0000

    Fix urls.py for Django 1.11

--- omero_signup/urls.py
+++ omero_signup/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    'django.views.generic.simple',
 
     # index 'home page' of the websignup app
     url(r'^$', views.WebSignupView.as_view(), name="signup_index"),
