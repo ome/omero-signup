3f52ff4e3572e739a25aa3bd2e72c860580b9c29
William Moore <w.moore@dundee.ac.uk>
2023-05-12 17:07:31 +0100

    urls.py uses re_path

--- omero_signup/urls.py
+++ omero_signup/urls.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 
 urlpatterns = [
 
     # index 'home page' of the websignup app
-    url(r'^$', views.WebSignupView.as_view(), name="signup_index"),
+    re_path(r'^$', views.WebSignupView.as_view(), name="signup_index"),
 ]
